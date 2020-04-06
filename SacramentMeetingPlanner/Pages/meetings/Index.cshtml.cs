1c49609ff35019dbefbd01e37b6382d5d4c0230c
Uchechukwu Ferdinand Nkenta <uchenkenta@gmail.com>
2020-04-06 12:14:15 +0100

    Print this page option added

--- SacramentMeetingPlanner/Pages/meetings/Index.cshtml.cs
+++ SacramentMeetingPlanner/Pages/meetings/Index.cshtml.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using SacramentMeetingPlanner.Data;
 using SacramentMeetingPlanner.Models;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
 
 namespace SacramentMeetingPlanner.Pages.meetings
 {
@@ -24,6 +25,36 @@ namespace SacramentMeetingPlanner.Pages.meetings
         public async Task OnGetAsync()
         {
             Meeting = await _context.Meeting.ToListAsync();
+        }
+
+        public IActionResult OnPostCreateDocument()
+        {
+            //Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+            //Add a page to the document
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page
+            _ = page.Graphics;
+
+            //Set the standard font
+            _ = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+
+            //Saving the PDF to the MemoryStream
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream);
+
+            //PDF will be empty if position is not set to '0'.
+            stream.Position = 0;
+
+            //Download the PDF file.
+            FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf")
+            {
+                FileDownloadName = "Meetings.pdf"
+            };
+            return fileStreamResult;
         }
     }
 }

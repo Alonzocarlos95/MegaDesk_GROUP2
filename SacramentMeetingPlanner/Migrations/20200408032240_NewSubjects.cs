9a10c249a9a23fbb4ed2307156101dea5f39fa42
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-04-07 22:25:09 -0500

    New Subjects

---
+++ SacramentMeetingPlanner/Migrations/20200408032240_NewSubjects.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SacramentMeetingPlanner.Migrations
+{
+    public partial class NewSubjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StakeAuthority",
+                table: "Meeting",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Presiding",
+                table: "Meeting",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pianist",
+                table: "Meeting",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MusicDirector",
+                table: "Meeting",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Leading",
+                table: "Meeting",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Speakersubjects2",
+                table: "Meeting",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Speakersubjects3",
+                table: "Meeting",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Speakersubjects2",
+                table: "Meeting");
+
+            migrationBuilder.DropColumn(
+                name: "Speakersubjects3",
+                table: "Meeting");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StakeAuthority",
+                table: "Meeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Presiding",
+                table: "Meeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pianist",
+                table: "Meeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MusicDirector",
+                table: "Meeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Leading",
+                table: "Meeting",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
